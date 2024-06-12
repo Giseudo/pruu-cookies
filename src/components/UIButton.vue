abe9a195dd6270ab8c600f300442ffc1cee8ca5c
Giseudo Oliveira <giseudo.oliveira@convenia.com.br>
2024-06-12 15:02:56 -0300

    created UI components

---
+++ src/components/UIButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="ui-button">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+.ui-button {
+  width: 200px;
+  height: 200px;
+  border-radius: 200px;
+  border: 0;
+  background: orange;
+  color: white;
+  font-size: 42px;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/UIMenu.vue
@@ -0,0 +1,60 @@
+<template>
+  <Transition name="menu">
+    <div v-show="isOpened" class="ui-menu" :style="{ top, left }">
+      <UIButton @click="emit('pigeon')">
+        Pigeon
+      </UIButton>
+
+      <UIButton @click="emit('cookie')">
+        Cookie
+      </UIButton>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+import { ref, watch, defineProps, defineEmits } from 'vue'
+import UIButton from './UIButton.vue'
+
+const left = ref('0px')
+const top = ref('0px')
+
+const props = defineProps({
+  isOpened: Boolean,
+
+  position: {
+    type: Object,
+    default: () => ({ x: 0, y: 0 })
+  }
+})
+
+watch(() => props.isOpened, (value) => {
+  if (!value)
+    return
+
+  top.value = `${props.position.y}px`
+  left.value = `${props.position.x}px`
+})
+
+const emit = defineEmits([ 'pigeon', 'cookie' ])
+</script>
+
+<style lang="scss">
+.ui-menu {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  display: flex;
+
+  .ui-button { margin: 20px; }
+}
+
+.menu-enter-active,
+.menu-leave-active {
+  transition: transform 0.15s cubic-bezier(0.64, 0.57, 0.67, 1.53);
+}
+
+.menu-enter-from,
+.menu-leave-to {
+  transform: translate(-50%, -50%) scale(0);
+}
+</style>
